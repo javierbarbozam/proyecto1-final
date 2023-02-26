dbf45055f9a6f8973947fa0850b8cad1c6896a02
IgnacioMG <ignaciomaringarro@gmail.com>
2023-02-25 19:23:52 -0600

    first commit of carousel
    I added a JS and CSS file for a carousel without content, and I also added the HTML for the entire design system

---
+++ main.js
@@ -0,0 +1,19 @@
+// Select the necessary items
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const carousel = document.querySelector(".carousel");
+const cardWidth = carousel.querySelector(".card").offsetWidth;
+
+// Calculate the horizontal displacement
+const cardsToShow = 6;
+const scrollDistance = cardWidth * cardsToShow;
+
+// Add event listeners to the navigation buttons
+prevBtn.addEventListener("click", function () {
+  carousel.scrollBy(-scrollDistance, 0);
+});
+
+nextBtn.addEventListener("click", function () {
+  carousel.scrollBy(scrollDistance, 0);
+});
+
