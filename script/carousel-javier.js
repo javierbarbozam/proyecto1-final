56d88925c9ff3dc598cfcfead2b605475896e09b
javierbarbozam <barboza.javier497@gmail.com>
2023-03-08 02:33:27 -0600

    Fix(carousel.scss) Creating a new styles structure

---
+++ script/carousel-javier.js
@@ -0,0 +1,18 @@
+// Select the necessary items
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const carousel = document.querySelector(".carousel");
+const cardWidth = carousel.querySelector(".carousel-item__title").offsetWidth;
+
+// Calculate the horizontal displacement
+const cardsToShow = 6;
+const scrollDistance = cardWidth * cardsToShow;
+
+// Add event listeners to the navigation buttons
+prevBtn.addEventListener("click", function () {
+  carousel.scrollBy(-scrollDistance, 0);
+});
+
+nextBtn.addEventListener("click", function () {
+  carousel.scrollBy(scrollDistance, 0);
+});
