422e7dba619ae42a7aad417ef7b3bb6a6e8e4f20
javierbarbozam <barboza.javier497@gmail.com>
2023-03-13 19:40:38 -0600

    practicing api code

--- script/indexApi.js
+++ script/indexApi.js
@@ -12,12 +12,10 @@ fetch('https://640b957794ce1239b09c0b5b.mockapi.io/api/destination', {
 		container.insertAdjacentHTML(
 			"beforeend",
 			`<li class="destination-item">
-				<img class="destination-item__img" src=${element.cardImage}>
+				<img class="destination-item__img" src="${element.cardImage}" alt="">
 				<span class="destination-item__title">${element.name}</span>
-				<a class=""><span class="visually-hidden">Click to see more</span></a>
+				<a class="destination-item__link" href="./destination/destination.html?id=${element.id}"><span class="visually-hidden">Click to see more</span></a>
 			</li>`
-
-			// need to add anchor url to go to every destination hotel (api?)
 		)
 	});	
   // Do something with the list of tasks
