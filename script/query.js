2f1bf7fec48c555eace67e926eac7df9d4c6168a
javierbarbozam <barboza.javier497@gmail.com>
2023-03-18 19:09:07 -0600

    Update query.js

--- script/query.js
+++ script/query.js
@@ -1,20 +1,43 @@
-
-const info_btn = document.querySelector('#js-btn-info');
-const card_btn = document.querySelector('#js-btn-card');
-const description = document.querySelector('#js-info-container');
-const cards = document.querySelector('#js-card-container');
+import { pageInfo, pageCards } from "./destinationApi.js";
 
 
-card_btn.addEventListener('click', () => {
-  description.classList.add('js-hidden');
-  description.classList.remove('js-visible');
-  cards.classList.remove('js-hidden');
-  cards.classList.add('js-visible');
-});
+const container = document.querySelector('#js-info-container');
 
-info_btn.addEventListener('click', () => {
-  description.classList.remove('js-hidden');
-  description.classList.add('js-visible');
-  cards.classList.remove('js-visible');
-  cards.classList.add('js-hidden');
-});
+// In order to have the first data form every destination, the function must be evoked
+pageInfo()
+
+// Destination buttons
+const btnFunctionality = () => {
+  const info_btn = document.querySelector('#js-btn-info');
+  const card_btn = document.querySelector('#js-btn-card');
+  info_btn.disabled = true;
+
+  card_btn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    //Clear data from container
+    container.innerHTML = '';
+    
+    //Call the function
+    pageCards()
+
+    //Disable button
+    info_btn.disabled = false;
+    card_btn.disabled = true;
+  });
+
+  info_btn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    //Clear data from container
+    container.innerHTML = '';
+    
+    //Call the function
+    pageInfo()
+
+    //Disable button
+    info_btn.disabled = true;
+    card_btn.disabled = false;
+  });
+}
+btnFunctionality()
