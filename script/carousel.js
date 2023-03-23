b020eea01e8cb2d485c5200b2d85bf330ae4f9d9
Darrellso <106450068+Darrellso@users.noreply.github.com>
2023-03-23 16:03:36 -0600

    corrections

--- script/carousel.js
+++ script/carousel.js
@@ -1,5 +1,12 @@
+// const carousel = document.querySelectorAll('.carousel')[0];   // carousel container, the one that will give the size of the screen
+// const carouselItem = document.querySelectorAll('.carousel-item')[0]; // The cards container, the one that will scroll to the left or right
+// const cards = document.querySelectorAll('.carousel-item__card')[0]; // Here I got the carousel cards from the DOM to access to width property
+// carouselBtn = document.querySelectorAll('.carousel-btn'); // Getting the carousel buttons to add functionability
+// let carouselWidth = carousel.offsetWidth; // Making a variable with the carousel container width 
+// let cardsWidth = cards.offsetWidth; // Making a variable with the carousel card width 
+
 // // This function verify the screen width and add the gap css style to the card, in order to have a simetric scroll
-// function resize () {
+// function resize() {
 //   carouselWidth = carousel.offsetWidth;
 //   cardsWidth = cards.offsetWidth;
 
@@ -8,35 +15,39 @@
 //     cardsWidth += 5;
 //   } else if (carouselWidth < 1024) {
 //     cardsWidth += 10;
-//   } else { cardsWidth += 20}
+//   } else { cardsWidth += 20 }
 // }
 // // First call of the function to have size of the elements once the browser has load the DOM
 // resize()
 // // Viewport event to have the card size always updated
-// window.addEventListener ('resize', resize);
+// window.addEventListener('resize', resize);
 
 // // Buttons functionability
 // carouselBtn.forEach(button => {
-//   button.addEventListener ('click', () => {
+//   button.addEventListener('click', () => {
 //     // This means that left btn will reduce width value from carrusel, and right btn will increase it.
 //     carouselItem.scrollLeft += button.classList[1] == 'carousel-btn--left' ? -cardsWidth : cardsWidth;
 //   })
 // })
 
 ///// DYNAMIC CONTENT
-const carousel = document.querySelectorAll('.carousel')[0],   // carousel container, the one that will give the size of the screen
-//const carouselItem = document.querySelectorAll('.carousel-item')[0], // The cards container, the one that will scroll to the left or right
-cards = document.querySelectorAll('.carousel-item__card')[0], // Here I got the carousel cards from the DOM to access to width property
-carouselBtn = document.querySelectorAll('.carousel-btn'); // Getting the carousel buttons to add functionability
-let carouselWidth = carousel.offsetWidth; // Making a variable with the carousel container width 
-let cardsWidth = cards.offsetWidth; // Making a variable with the carousel card width 
-const carouselItem = document.querySelector('.carousel-item') // The cards container, the one that will scroll to the left or right
+
+
+
+let carousel = document.querySelector('.carousel'); // carousel container, the one that will give the size of the screen
+let cards = document.querySelectorAll('.carousel-item__card'); // Card width property
+let carouselItem = document.querySelectorAll('.carousel-item')[0]; // The cards container, the one that will scroll to the left or right
+let carouselWidth = carousel.offsetWidth; // Variable with carousel container width 
+let cardsWidth = cards.offsetWidth; // Variable with card width
+
 
 
 const carouselCard = () => {
+
+
   fetch('https://640b957794ce1239b09c0b5b.mockapi.io/api/hotel?highlight=true', {
     method: 'GET',
-    headers: {'content-type':'application/json'},
+    headers: { 'content-type': 'application/json' },
   }).then(res => {
     if (res.ok) {
       return res.json();
@@ -45,58 +56,52 @@ const carouselCard = () => {
     let cardsInfo = '';
     element.forEach(element => {
       cardsInfo +=
-      `<li class="carousel-item__card">
-        <img class="carousel-item_card_img" src="./${element.image}" alt="">
-        <span class="carousel-item_card_title">${element.name}</span>
-        <a href="./hotel.hotel.html?search=${element.name}" class="carousel-item_card_link"><span class="visually-hidden">Show More</span></a>
+        `<li class="carousel-item__card">
+        <img class="carousel-item__card__img" src="./${element.image}" alt="">
+        <span class="carousel-item__card__title">${element.name}</span>
+        <a href="./hotel.hotel.html?search=${element.name}" class="carousel-item__card__link"><span class="visually-hidden">Show More</span></a>
       </li>`
     })
 
-    carouselItem.insertAdjacentHTML (
+
+    carouselItem.insertAdjacentHTML(
       'afterbegin',
       `${cardsInfo}`
     )
 
     const carouselFunctionality = () => {
 
-      const carousel = document.querySelector('.carousel'); // carousel container, the one that will give the size of the screen
-      const cards = document.querySelectorAll('.carousel-item__card'); // Card width property
-      let carouselWidth = carousel.offsetWidth; // Variable with carousel container width 
-      let cardsWidth = cards.offsetWidth; // Variable with card width
-
-      // This function verify the screen width and add the gap css style to the card, in order to have a simetric scroll
-      function resize () {
-        carouselWidth = carousel.offsetWidth;
-        cardsWidth = cards.offsetWidth;
-
-         // According to the media querry, the gap changes, so this part is made to add the correct amount to the card with
-        if (carouselWidth < 480) {
-          cardsWidth += 5;
-        } else if (carouselWidth < 1024) {
-          cardsWidth += 10;
-        } else { cardsWidth += 20}
-      }
-        
-      // First call of the function to have size of the elements once the browser has load the DOM
-      resize()
-      // Viewport event to have the card size always updated
-      window.addEventListener ('resize', resize);
-
-    
-
     }
-      // Buttons functionability
-    const carouselBtn = document.querySelectorAll('.carousel-btn');
-    carouselBtn.forEach(button => {
-      button.addEventListener ('click', () => {
-        // This means that left btn will reduce width value from carrusel, and right btn will increase it.
-      carouselItem.scrollLeft += button.classList[1] == 'carousel-btn--left' ? -cardsWidth : cardsWidth;
-      })
-    })
 
     carouselFunctionality()
   }).catch(error => {
-   // handle error
+    // handle error
   })
 }
 carouselCard()
+// This function verify the screen width and add the gap css style to the card, in order to have a simetric scroll
+function resize() {
+  carouselWidth = carousel.offsetWidth;
+  cardsWidth = cards.offsetWidth;
+
+  // According to the media querry, the gap changes, so this part is made to add the correct amount to the card with
+  if (carouselWidth < 480) {
+    cardsWidth += 5;
+  }
+  // else if (carouselWidth < 1024) {
+  //   cardsWidth += 10;
+  // } else { cardsWidth += 20 }
+}
+// First call of the function to have size of the elements once the browser has load the DOM
+resize()
+// Viewport event to have the card size always updated
+window.addEventListener('resize', resize);
+
+// Buttons functionability
+const carouselBtn = document.querySelectorAll('.carousel-btn');
+carouselBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    // This means that left btn will reduce width value from carrusel, and right btn will increase it.
+    carouselItem.scrollLeft += button.classList[1] == 'carousel-btn--left' ? -cardsWidth : cardsWidth;
+  })
+})
